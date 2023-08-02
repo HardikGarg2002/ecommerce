13ebd3c993d82664b3084b8f606cd88d301937d3
hardikgarg2002 <hardikgarg3085@gmail.com>
2023-08-02 12:00:00 +0530

    setting up typescript and its configuration

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
-import express from "express";
+import createServer from "./src/app";
 
-const app = express();
+const app = createServer();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req: Request, res: Response) => {
