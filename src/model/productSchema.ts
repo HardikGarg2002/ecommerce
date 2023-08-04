794633f5c572da63a208af62dfc96238ab9ac41a
hardikgarg2002 <hardikgarg3085@gmail.com>
2023-08-04 12:00:00 +0530

    timestamps and audit details added in product schema

--- src/model/productSchema.ts
+++ src/model/productSchema.ts
@@ -1,38 +1,45 @@
 import mongoose from "mongoose";
 
-const productSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: true,
-    maxlength: 200,
-  },
-  description: {
-    type: String,
-    trim: true,
-    maxlength: 2000,
-  },
-  cost: {
-    type: Number,
-    required: true,
-  },
-  shortDesc: {
-    type: String,
-    maxlength: 400,
-  },
-  images: {
-    primary: {
+const productSchema = new mongoose.Schema(
+  {
+    name: {
       type: String,
       trim: true,
+      required: true,
+      maxlength: 200,
     },
-    additional: [
-      {
+    desc: {
+      type: String,
+      trim: true,
+      maxlength: 2000,
+    },
+    cost: {
+      type: Number,
+      required: true,
+    },
+    short_desc: {
+      type: String,
+      maxlength: 400,
+    },
+    images: {
+      primary: {
         type: String,
         trim: true,
-        default: [],
       },
-    ],
+      additional: [
+        {
+          type: String,
+          trim: true,
+          default: [],
+        },
+      ],
+    },
+    createdBy: String,
+    updatedBy: String,
   },
-});
+  {
+    timestamps: true,
+  }
+);
 
 export default mongoose.model("Product", productSchema);
