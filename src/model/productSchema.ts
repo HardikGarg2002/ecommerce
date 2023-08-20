1c3702b195fbde0a77c34a1ce0d10d339dc898a9
hardikgarg2002 <hardikgarg3085@gmail.com>
2023-08-20 12:30:05 +0530

    brand integrated with product

--- src/model/productSchema.ts
+++ src/model/productSchema.ts
@@ -45,6 +45,11 @@ const productSchema = new mongoose.Schema(
         trim: true,
       },
     ],
+    brand: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Brand",
+      required: true,
+    },
     createdBy: String,
     updatedBy: String,
   },
