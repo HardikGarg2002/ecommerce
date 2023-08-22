debc1cdf46abb2a32d7c2db671fa7c4ab18203de
hardikgarg2002 <hardikgarg3085@gmail.com>
2023-08-22 12:30:05 +0530

    user schema

---
+++ src/model/user.ts
@@ -0,0 +1,27 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+export default mongoose.model("User", userSchema);
