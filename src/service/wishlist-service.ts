7e05e204ad6530536306b037d59e1d944e12f763
hardikgarg2002 <hardikgarg3085@gmail.com>
2023-09-02 12:30:00 +0530

    validations for create wishlist by user id

--- src/service/wishlist-service.ts
+++ src/service/wishlist-service.ts
@@ -2,6 +2,17 @@ import Wishlist from "../model/wishlist";
 
 async function createWishlist(wishlist: any) {
   try {
+    if (!wishlist.userId) {
+      throw { message: "userId is required" };
+    }
+    const existingWishlist = await Wishlist.findOne({
+      userId: wishlist.userId,
+    });
+    if (existingWishlist) {
+      console.log("wishlist already exists for this user");
+      existingWishlist.products = wishlist.products;
+      return await existingWishlist.save();
+    }
     const newWishlist = new Wishlist(wishlist);
     return await newWishlist.save();
   } catch (error) {
