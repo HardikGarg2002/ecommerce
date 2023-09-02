e290985328b36d1c1aab06147c25869cc0956c55
hardikgarg2002 <hardikgarg3085@gmail.com>
2023-09-03 04:30:35 +0530

    generate and vrify otp

--- src/service/auth-service.ts
+++ src/service/auth-service.ts
@@ -2,6 +2,9 @@ import User from "../model/user";
 import bcrypt from "bcrypt";
 import { generateToken } from "../utils/GenerateToken";
 import { sanitizeUser } from "../utils/sanitizeUser";
+import Otp from "../model/otp";
+import { generateOTP } from "../utils/GenerateOtp";
+import { sendMail } from "../utils/Notification";
 
 async function signup(userInput: any) {
   try {
@@ -50,4 +53,80 @@ async function login(userInput: any) {
   }
 }
 
-export { signup, login };
+async function verifyOtp(req: any, res: any) {
+  try {
+    const isValidUserId = await User.findById(req.body.userId);
+
+    if (!isValidUserId) {
+      return res.status(404).json({
+        message: "User not Found, for which the otp has been generated",
+      });
+    }
+
+    const isOtpExisting = await Otp.findOne({ user: isValidUserId._id });
+
+    if (!isOtpExisting) {
+      return res.status(404).json({ message: "Otp not found" });
+    }
+
+    if (isOtpExisting.expiresAt < new Date()) {
+      await Otp.findByIdAndDelete(isOtpExisting._id);
+      return res.status(400).json({ message: "Otp has been expired" });
+    }
+
+    if (
+      isOtpExisting &&
+      (await bcrypt.compare(req.body.otp, isOtpExisting.otp))
+    ) {
+      await Otp.findByIdAndDelete(isOtpExisting._id);
+      const verifiedUser = await User.findByIdAndUpdate(
+        isValidUserId._id,
+        { isVerified: true },
+        { new: true }
+      );
+      return res.status(200).json(sanitizeUser(verifiedUser));
+    }
+
+    return res.status(400).json({ message: "Otp is invalid or expired" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Some Error occurred" });
+  }
+}
+
+async function resendOtp(userId: string) {
+  try {
+    const existingUser = await User.findById(userId);
+
+    if (!existingUser) {
+      throw new Error("User not found");
+    }
+
+    await Otp.deleteMany({ user: existingUser._id });
+
+    const otp = generateOTP();
+    const hashedOtp = await bcrypt.hash(otp, 10);
+
+    const newOtp = new Otp({
+      user: userId,
+      otp: hashedOtp,
+      expiresAt:
+        Date.now() + parseInt(process.env.OTP_EXPIRATION_TIME || "600"),
+    });
+    await newOtp.save();
+    await sendMail(
+      existingUser.email,
+      `OTP Verification for Your Account`,
+      `Your One-Time Password (OTP) for account verification is: <b>${otp}</b>.</br>Do not share this OTP with anyone for security reasons`
+    );
+
+    return { message: "OTP sent" };
+  } catch (error) {
+    console.log(error);
+    throw new Error(
+      "Some error occurred while resending otp, please try again later"
+    );
+  }
+}
+
+export { signup, login, verifyOtp, resendOtp };
