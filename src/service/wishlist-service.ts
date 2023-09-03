bb5403234cd4193048be066c8415f790fd32cd8e
hardikgarg2002 <hardikgarg3085@gmail.com>
2023-09-03 12:30:00 +0530

    wishlist service layer functions

--- src/service/wishlist-service.ts
+++ src/service/wishlist-service.ts
@@ -21,6 +21,15 @@ async function createWishlist(wishlist: any) {
   }
 }
 
+async function getAllWishlists() {
+  try {
+    return await Wishlist.find();
+  } catch (error) {
+    console.log("error", error);
+    throw error;
+  }
+}
+
 async function getWishlistByUserId(userId: string) {
   try {
     return await Wishlist.find({ userId });
@@ -30,4 +39,30 @@ async function getWishlistByUserId(userId: string) {
   }
 }
 
-export { createWishlist, getWishlistByUserId };
+async function updateWishlist(wishlistId: string, wishlist: any) {
+  try {
+    return await Wishlist.findByIdAndUpdate(wishlistId, wishlist, {
+      new: true,
+    });
+  } catch (error) {
+    console.log("error", error);
+    throw error;
+  }
+}
+
+async function deleteWishlist(wishlistId: string) {
+  try {
+    return await Wishlist.findByIdAndDelete(wishlistId);
+  } catch (error) {
+    console.log("error", error);
+    throw error;
+  }
+}
+
+export {
+  createWishlist,
+  getAllWishlists,
+  getWishlistByUserId,
+  updateWishlist,
+  deleteWishlist,
+};
