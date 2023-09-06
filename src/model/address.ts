fc24c8373c955706bba6a1071a10825f0a2f48e6
hardikgarg2002 <hardikgarg3085@gmail.com>
2023-09-06 14:30:00 +0530

    Added address schema

---
+++ src/model/address.ts
@@ -0,0 +1,39 @@
+import mongoose, { Schema } from "mongoose";
+
+const addressSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  street: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  state: {
+    type: String,
+    required: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+  },
+  postalCode: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+});
+
+export default mongoose.model("Address", addressSchema);
