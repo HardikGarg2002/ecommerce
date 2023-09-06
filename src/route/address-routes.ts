adc792b36149c4996eb9011b2cd740edc8c16146
hardikgarg2002 <hardikgarg3085@gmail.com>
2023-09-06 15:33:00 +0530

    address routing

--- src/route/address-routes.ts
+++ src/route/address-routes.ts
@@ -3,8 +3,28 @@ import * as addressService from "../service/address-service";
 
 const router = express.Router();
 
-router
-  .post("/", addressService.create)
-  .get("/user/:id", addressService.getByUserId);
+router.post("/", async (req, res) => {
+  try {
+    const created = await addressService.create(req.body);
+    res.status(201).json(created);
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ message: "Error creating an address, please try again later" });
+  }
+});
+
+router.get("/user/:id", async (req, res) => {
+  try {
+    const results = await addressService.getByUserId(req.params.id);
+    res.status(200).json(results);
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ message: "Error fetching address, please try again later" });
+  }
+});
 
 export default router;
