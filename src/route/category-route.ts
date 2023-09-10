fefc2509fcd096f2c0dead63d87ceb01d2eeaa60
hardikgarg2002 <hardikgarg3085@gmail.com>
2023-09-10 22:30:00 +0530

    category routing restructured

--- src/route/category-route.ts
+++ src/route/category-route.ts
@@ -4,18 +4,56 @@ import * as categoryService from "../service/category-service";
 const router = express.Router();
 
 // Route to create a new category
-router.route("/").post(categoryService.createCategory);
+router.route("/").post(async (req, res) => {
+  try {
+    const category = await categoryService.createCategory(req.body);
+    res.json(category);
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+});
 
 // Route to get all categories
-router.route("/").get(categoryService.getAllCategories);
+router.route("/").get(async (req, res) => {
+  try {
+    const categories = await categoryService.getAllCategories();
+    res.json(categories);
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+});
 
 // Route to get category by id
-router.route("/:id").get(categoryService.getCategoryById);
+router.route("/:id").get(async (req, res) => {
+  try {
+    const category = await categoryService.getCategoryById(req.params.id);
+    res.json(category);
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+});
 
 // Route to update category by id
-router.route("/:id").put(categoryService.updateCategory);
+router.route("/:id").put(async (req, res) => {
+  try {
+    const category = await categoryService.updateCategory(
+      req.params.id,
+      req.body
+    );
+    res.json(category);
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+});
 
 // Route to delete category by id
-router.route("/:id").delete(categoryService.deleteCategory);
+router.route("/:id").delete(async (req, res) => {
+  try {
+    const category = await categoryService.deleteCategory(req.params.id);
+    res.json(category);
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+});
 
 export default router;
