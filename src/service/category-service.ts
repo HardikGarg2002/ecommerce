d65fb6ffa2045de7e0faeb698552548c7437b06b
hardikgarg2002 <hardikgarg3085@gmail.com>
2023-09-17 19:03:00 +0530

    refactor category

--- src/service/category-service.ts
+++ src/service/category-service.ts
@@ -1,50 +1,25 @@
 import Category from "../model/category";
 
 export async function create(category: any) {
-  try {
-    const newCategory = new Category(category);
-    return await newCategory.save();
-  } catch (error) {
-    console.log("error", error);
-    throw error;
-  }
+  const newCategory = new Category(category);
+  return await newCategory.save();
 }
 
 export async function get() {
-  try {
-    return await Category.find();
-  } catch (error) {
-    console.log("error", error);
-    throw error;
-  }
+  return await Category.find();
 }
 
 export async function getById(categoryId: string) {
-  try {
-    return await Category.findById(categoryId);
-  } catch (error) {
-    console.log("error", error);
-    throw error;
-  }
+  return await Category.findById(categoryId);
 }
 
 export async function patch(categoryId: string, category: any): Promise<void> {
-  try {
-    await Category.findByIdAndUpdate(categoryId, category);
-  } catch (error) {
-    console.log("error", error);
-    throw error;
-  }
+  await Category.findByIdAndUpdate(categoryId, category);
 }
 
 export async function patchStatus(
   categoryId: string,
   is_active: boolean
 ): Promise<void> {
-  try {
-    await Category.findByIdAndUpdate(categoryId, { is_active });
-  } catch (error) {
-    console.log("error", error);
-    throw error;
-  }
+  await Category.findByIdAndUpdate(categoryId, { is_active });
 }
