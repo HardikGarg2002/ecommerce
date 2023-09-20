e0269b3f072debdd59fef37b7213d977bac3e4b6
hardikgarg2002 <hardikgarg3085@gmail.com>
2023-09-20 15:03:00 +0530

    refactor address

--- src/service/address-service.ts
+++ src/service/address-service.ts
@@ -1,24 +1,14 @@
 import Address from "../model/address";
 
 export const create = async (addressInput: any) => {
-  try {
-    if (!addressInput) {
-      throw new Error("Address input is required");
-    }
-    const address = new Address(addressInput);
-    return await address.save();
-  } catch (error) {
-    console.log(error);
-    throw error;
+  if (!addressInput) {
+    throw new Error("Address input is required");
   }
+  const address = new Address(addressInput);
+  return await address.save();
 };
 
 export const getByUserId = async (userId: string) => {
-  try {
-    const result = await Address.find({ user: userId });
-    return result;
-  } catch (error) {
-    console.log(error);
-    throw error;
-  }
+  const result = await Address.find({ user: userId });
+  return result;
 };
