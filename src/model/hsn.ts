3624fc6b331be86761e3fd5b0fbead1a46a10c1e
hardikgarg2002 <hardikgarg3085@gmail.com>
2023-09-21 20:40:00 +0530

    tag apis

---
+++ src/model/hsn.ts
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+
+const hsnSchema = new mongoose.Schema({
+  code: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    index: true,
+  },
+  desc: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  gst: {
+    type: Number,
+    required: true,
+  },
+  is_active: {
+    type: Boolean,
+    default: true,
+  },
+  created: {
+    _id: {
+      type: mongoose.Schema.Types.ObjectId,
+      immutable: true,
+    },
+    name: {
+      type: String,
+      immutable: true,
+    },
+    date: {
+      type: Date,
+      default: Date.now,
+      immutable: true,
+    },
+  },
+  updated: {
+    _id: {
+      type: mongoose.Schema.Types.ObjectId,
+    },
+    name: {
+      type: String,
+    },
+    date: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+});
+
+export default mongoose.model("hsns", hsnSchema);
