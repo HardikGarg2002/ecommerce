2b8cce7fea18f3b3f1b48faba572a154c91437de
hardikgarg2002 <hardikgarg3085@gmail.com>
2024-05-14 09:30:00 +0530

    defining customer interface

---
+++ src/interface/customer.ts
@@ -0,0 +1,23 @@
+import { IPagination } from "./pagination";
+import { IAddress } from "./address";
+
+export enum CustomerStatus {
+  ACTIVE = "ACTIVE",
+  BLACKLISTED = "BLACKLISTED",
+}
+
+export default interface ICustomer {
+  _id?: string;
+  auth_id: string;
+  name: string;
+  mobile: string;
+  email?: string;
+  address?: IAddress[];
+  creation_date?: Date;
+  status?: CustomerStatus;
+}
+
+export interface ICustomerWithMeta {
+  data: ICustomer[];
+  meta: IPagination;
+}
