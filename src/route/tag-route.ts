2966a173919233ad71d62f69b85a97455d4c21ba
hardikgarg2002 <hardikgarg3085@gmail.com>
2024-05-16 14:40:00 +0530

    tag get apis

--- src/route/tag-route.ts
+++ src/route/tag-route.ts
@@ -12,3 +12,25 @@ router.route("/").post((req, res) => {
     res.status(500).json({ message: error });
   }
 });
+
+//Route to get all tags
+router.route("/").get((req, res) => {
+  try {
+    const allTags = tagHandler.get();
+    res.send(allTags);
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+});
+
+//Route to get a tag by id
+router.route("/:id").get((req, res) => {
+  try {
+    const tag = tagHandler.getById(req.params.id);
+    res.json(tag);
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+});
+
+export default router;
--- src/service/tag-service.ts
+++ src/service/tag-service.ts
@@ -4,3 +4,15 @@ export async function create(tag: any) {
   const newTag = new Tag(tag);
   return await newTag.save();
 }
+
+export async function get() {
+  return await Tag.find();
+}
+
+export async function getById(tagId: string) {
+  const tag = await Tag.findById(tagId);
+  if (!tag) {
+    throw new Error("Tag not found");
+  }
+  return tag;
+}
