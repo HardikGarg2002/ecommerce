e0bd4da4a9aec85b5e492f81a82f935a27e6c09b
hardikgarg2002 <hardikgarg3085@gmail.com>
2024-05-20 23:30:00 +0530

    feat: add get  Subcategory APIs with route and service

--- src/route/subcategory-route.ts
+++ src/route/subcategory-route.ts
@@ -10,3 +10,13 @@ router.post("/", async (req, res) => {
     res.status(500).json({ message: error });
   }
 });
+
+router.get("/", (req, res) => {
+  const allSubcategories = subcategoryService.get();
+  res.send(allSubcategories);
+});
+
+router.get("/:id", (req, res) => {
+  const subcategory = subcategoryService.getById(req.params.id);
+  res.send(subcategory);
+});
--- src/service/subcategory-service.ts
+++ src/service/subcategory-service.ts
@@ -4,3 +4,15 @@ export async function create(subcategory: any) {
   const newSubcategory = new Subcategory(subcategory);
   return await newSubcategory.save();
 }
+
+export async function get() {
+  return await Subcategory.find();
+}
+
+export async function getById(subcategoryId: string) {
+  const subcategory = await Subcategory.findById(subcategoryId);
+  if (!subcategory) {
+    throw new Error("Subcategory not found");
+  }
+  return subcategory;
+}
