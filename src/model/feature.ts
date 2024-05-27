94cd1401a73abd3500a6775f756ece51d6b8c0d7
hardikgarg2002 <hardikgarg3085@gmail.com>
2024-05-27 16:45:00 +0530

    feat: feature schema and integrated with its interface

---
+++ src/model/feature.ts
@@ -0,0 +1,70 @@
+import mongoose, { Schema } from "mongoose";
+import { IFeature } from "../interface/feature";
+
+const featureSchema = new Schema<IFeature>({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+
+  desc: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+
+  code: {
+    type: String,
+    index: true,
+    uppercase: true,
+    immutable: true,
+    trim: true,
+  },
+
+  type: {
+    type: String,
+    uppercase: true,
+    trim: true,
+  },
+
+  created: {
+    _id: {
+      type: String,
+      immutable: true,
+    },
+    name: {
+      type: String,
+      immutable: true,
+    },
+    date: {
+      type: Date,
+      immutable: true,
+      default: Date.now,
+    },
+  },
+
+  updated: {
+    _id: {
+      type: String,
+    },
+    name: {
+      type: String,
+    },
+    date: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+
+  sort: {
+    type: Number,
+    required: true,
+    default: 100,
+  },
+});
+
+const Feature = mongoose.model<IFeature>("features", featureSchema);
+
+export default Feature;
