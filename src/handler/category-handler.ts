0ab823ce67ae6f7eb0ab05b0ee8ac26df388a2a7
hardikgarg2002 <hardikgarg3085@gmail.com>
2024-05-29 18:45:00 +0530

    Refactor: Move input handling and response logic to new layer handler
    - Introduced a new layer handler that collects input from the request and sends the response.
    - This logic was previously handled in the route layer.
    - The change improves separation of concerns and enhances maintainability by modularizing the responsibilities.

---
+++ src/handler/category-handler.ts
@@ -0,0 +1,77 @@
+import { Request, Response, NextFunction } from "express";
+import * as CategoryService from "../service/category-service";
+import { IUser } from "../interface/user";
+import { ICategory } from "../interface/category";
+// const categoryController = new CategoryController();
+
+export const create = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const user: IUser = req.body.loggedInUser;
+    const categoryInput: ICategory = req.body;
+    categoryInput.createdBy = user.name;
+
+    categoryInput.updatedBy = categoryInput.createdBy;
+    const id = await CategoryService.create(categoryInput);
+    res.status(201).json({ message: "category created successfully", id });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const get = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const categories = await CategoryService.get();
+    res.status(200).json(categories);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getById = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const catId = req.params.id;
+    const category = await CategoryService.getById(catId);
+    res.status(200).json(category);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const patch = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const user: IUser = req.body.loggedInUser;
+    const catId = req.params.id;
+    const { name, desc, img_url, sort } = req.body;
+    await CategoryService.patch(catId, { user, name, desc, img_url, sort });
+    res.status(200).json({ message: "category updated successfully" });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const patchStatus = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const catId = req.params.id;
+    const { is_active } = req.body;
+    await CategoryService.patchStatus(catId, is_active);
+    res.status(200).json({ message: "category status updated successfully" });
+  } catch (err) {
+    next(err);
+  }
+};
--- src/route/category-route.ts
+++ src/route/category-route.ts
@@ -1,60 +1,27 @@
 import express from "express";
-import * as categoryService from "../service/category-service";
+import * as categoryHandler from "../handler/category-handler";
 
 const router = express.Router();
 
 // Route to create a new category
-router.route("/").post(async (req, res) => {
-  try {
-    const category = await categoryService.create(req.body);
-    res.json(category);
-  } catch (error) {
-    res.status(500).json({ message: error });
-  }
-});
+router.route("/").post(categoryHandler.create);
 
-// Route to get all categories
-router.route("/").get(async (req, res) => {
-  try {
-    const categories = await categoryService.get();
-    res.json(categories);
-  } catch (error) {
-    res.status(500).json({ message: error });
-  }
-});
+//Route to get all categories
+router.route("/").get(categoryHandler.get);
 
-// Route to get category by id
-router.route("/:id").get(async (req, res) => {
-  try {
-    const category = await categoryService.getById(req.params.id);
-    res.json(category);
-  } catch (error) {
-    res.status(500).json({ message: error });
-  }
-});
+//Route to get all categories based on searchText
+// router.route("/search").get(categoryHandler.search);
 
-// Route to update category by id
-router.route("/:id").put(async (req, res) => {
-  try {
-    const category = await categoryService.patch(req.params.id, req.body);
-    res.json(category);
-  } catch (error) {
-    res.status(500).json({ message: error });
-  }
-});
+//Route to get a category by id
+router.route("/:id").get(categoryHandler.getById);
 
-// Route to delete category by id
-router.route("/:id").delete(async (req, res) => {
-  try {
-    const is_active = false;
-    const category = await categoryService.patchStatus(
-      req.params.id,
-      is_active
-    );
-    res.json(category);
-  } catch (error) {
-    res.status(500).json({ message: error });
-  }
-});
+//Route to edit a category by id (only active) (also send meta data for the affected fields future prospective)
+router.route("/:id").patch(categoryHandler.patch);
+
+//Route to delete a category by id (not assigned)    hard delete
+// router.route("/:id").delete(categoryHandler.remove);
+
+// Route to edit a status by id (any) (also send meta data for the affected fields)
+router.route("/:id/status").patch(categoryHandler.patchStatus);
 
 export default router;
