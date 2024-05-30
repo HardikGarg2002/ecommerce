f7b84fd90fb8f91ab6ba030d6d6c382917f7530d
hardikgarg2002 <hardikgarg3085@gmail.com>
2024-05-30 09:45:00 +0530

    Refactor: TAG  : Move input handling and response logic to new layer handler
    - Introduced a new layer handler that collects input from the request and sends the response.
    - This logic was previously handled in the route layer.
    - The change improves separation of concerns and enhances maintainability by modularizing the responsibilities.

---
+++ src/handler/tag-handler.ts
@@ -0,0 +1,61 @@
+import { NextFunction, Request, Response } from "express";
+import * as tagService from "../service/tag-service";
+import { IUser } from "../interface/user";
+import { ITag } from "../interface/tag";
+// const tagController = new TagController();
+
+export const create = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const user: IUser = req.body.loggedInUser;
+    const tagInput: ITag = req.body;
+    tagInput.createdBy = user.name;
+    tagInput.updatedBy = tagInput.createdBy;
+    const _id = await tagService.create(tagInput);
+    res.status(200).json({ message: "Tag created Successfully", id: _id });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const get = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const tags = await tagService.get();
+    res.status(200).json(tags);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getById = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const id: string = req.params.id;
+    const tag = await tagService.getById(id);
+    res.status(200).json(tag);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const activate = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const user: IUser = req.body.loggedInUser;
+    const id: string = req.params.id;
+    const { is_active } = req.body;
+    await tagService.patchStatus(id, is_active);
+    res.status(200).json({ message: "tag updated successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
--- src/route/tag-route.ts
+++ src/route/tag-route.ts
@@ -1,70 +1,20 @@
 import express from "express";
-import * as tagHandler from "../service/tag-service";
-
+import * as tagHandler from "../handler/tag-handler";
 const router = express.Router();
 
-// Route to create a new tag
-router.route("/").post((req, res) => {
-  try {
-    const tag = tagHandler.create(req.body);
-    res.json(tag);
-  } catch (error) {
-    res.status(500).json({ message: error });
-  }
-});
+//route to create new tag
+router.route("/").post(tagHandler.create);
 
-//Route to get all tags
-router.route("/").get((req, res) => {
-  try {
-    const allTags = tagHandler.get();
-    res.send(allTags);
-  } catch (error) {
-    res.status(500).json({ message: error });
-  }
-});
+// route to get all tag
+router.route("/").get(tagHandler.get);
 
-//Route to get a tag by id
-router.route("/:id").get((req, res) => {
-  try {
-    const tag = tagHandler.getById(req.params.id);
-    res.json(tag);
-  } catch (error) {
-    res.status(500).json({ message: error });
-  }
-});
+//Route to get all tags based on searchText
+// router.route("/search").get( tagHandler.search);
 
-//Route to edit a tag by id (only active) (also send meta data for the affected fields future prospective)
-router.route("/:id").patch((req, res) => {
-  try {
-    const tag = req.body;
-    const id = req.params.id;
-    const updatedTag = tagHandler.patch(id, tag);
-    res.send("Tag Updated Successfully");
-  } catch (error) {
-    res.status(500).json({ message: error });
-  }
-});
+//route to get a tag by ID
+router.route("/:id").get(tagHandler.getById);
 
-//Route to delete a tag by id (not assigned)    hard delete
-router.route("/:id").delete((req, res) => {
-  try {
-    const id = req.params.id;
-    const deletedTag = tagHandler.remove(id);
-    res.send("Tag Deleted Successfully");
-  } catch (error) {
-    res.status(500).json({ message: error });
-  }
-});
-
-// Route to edit a status by id (any) (also send meta data for the affected fields)
-router.route("/:id/status").patch((req, res) => {
-  try {
-    const status = req.body.status;
-    const tag = tagHandler.patchStatus(req.params.id, status);
-    res.json(tag);
-  } catch (error) {
-    res.status(500).json({ message: error });
-  }
-});
+//route to edit text of tag
+router.route("/:id/status").patch(tagHandler.activate);
 
 export default router;
