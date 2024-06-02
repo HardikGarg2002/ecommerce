8dfd17040630010e3adf5829b3c7dfa763d8a7b9
hardikgarg2002 <hardikgarg3085@gmail.com>
2024-06-02 14:30:00 +0530

    feat: customer interface for frontend order creation

---
+++ src/model/customer.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import ICustomer, { CustomerStatus } from "../interface/customer";
+import { addressSchema } from "./address";
+
+const customerSchema = new mongoose.Schema<ICustomer>({
+  _id: { type: mongoose.Schema.Types.ObjectId },
+  auth_id: { type: String, required: true, unique: true },
+  name: { type: String, trim: true },
+  mobile: { type: String, unique: true, required: true },
+  email: { type: String },
+  address: [addressSchema],
+  status: {
+    type: String,
+    enum: CustomerStatus,
+    default: CustomerStatus.ACTIVE,
+  },
+  creation_date: { type: Date, default: Date.now },
+});
+
+const Customer = mongoose.model<ICustomer>("Customers", customerSchema);
