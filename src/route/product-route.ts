f3235af4d2add63909f17da6032c1599e2f6fdba
hardikgarg2002 <hardikgarg3085@gmail.com>
2024-06-02 14:30:00 +0530

    refactor:product routing

--- src/route/product-route.ts
+++ src/route/product-route.ts
@@ -1,40 +1,57 @@
-import * as productService from "../service/product-service";
 import express from "express";
+import * as productHandler from "../handler/product-handler";
 
 const router = express.Router();
 
-// api to create product
-router.post("/", async (req, res) => {
-  try {
-    const product = await productService.create(req.body);
-    res.json(product);
-  } catch (error) {
-    res.status(500).json({ message: error });
-  }
-});
+// Route to get all products with filters and pagination (for active and inactive also)
+router.route("/").get(productHandler.get);
+//get products for an array of product IDs
+router.get("/ids", productHandler.getbyIds);
+// Route to create a new product
+router.route("/").post(productHandler.create);
+// search permission with filter pagination and sort and with searchText applied on name and desc
+router.route("/search").get(productHandler.search);
 
-router.get("/", (req, res) => {
-  const allProducts = productService.get();
-  res.send(allProducts);
-});
+// Route to get a product by id
+router.route("/:id").get(productHandler.getById);
+// Route to edit a product by id (only active) (also send meta data for the affected fields future prospective)
+router.route("/:id").patch(productHandler.patch);
+// Route to edit a status by id (any) (also send meta data for the affected fields)
+router.route("/:id/status").patch(productHandler.activate);
 
-router.get("/:id", (req, res) => {
-  const Products = productService.getById(req.params.id);
-  console.log(Products, req.params.id);
-  res.send(Products);
-});
+// Route to edit an offer on product status
+router.route("/:id/offer").patch(productHandler.patchOffer);
+// Route to edit an offer on product prices
+router.route("/:id/price").patch(productHandler.patchPrice);
+// Route to edit a stock availability status
+router.route("/:id/oos").patch(productHandler.patchOos);
 
-router.patch("/:id", (req, res) => {
-  const product = req.body;
-  const id = req.params.id;
-  const updatedProduct = productService.update(id, product);
-  res.send("Product Updated Successfully");
-});
+// Route to add a tag with id in the product with a specific product id
+router.route("/:id/tags").post(productHandler.addTag);
+// Route to remove the tag associated with a specific product id
+router.route("/:id/tags").delete(productHandler.removeTag);
 
-router.delete("/:id", (req, res) => {
-  const id = req.params.id;
-  const deletedProduct = productService.patchStatus(id, false);
-  res.send("Product Deleted Successfully");
-});
+// Route to add multiple images inside the product
+router.route("/:id/images").post(productHandler.addImages);
+
+// Route to add single or multiple aliases to the product
+router.route("/:id/aliases").post(productHandler.addAliases);
+//Route to remove alias of a product
+router.route("/:id/aliases/:alias_id").delete(productHandler.removeAlias);
+// // Route to remove img_url from the product
+router.route("/:id/images/:img_url").delete(productHandler.removeImage);
+// route for setting any image as primary
+
+// Route to add single or multiple features to the product
+router.route("/:id/features").post(productHandler.addFeatures);
+//Route to remove a particular feature from array of features
+router.route("/:id/features/:code").delete(productHandler.removeFeature);
+
+// Route to add multiple relatedProduct to the product
+router.route("/:id/relatedproducts").post(productHandler.addRelatedProduct);
+// Route to remove a particular related product from array of related product ids
+router
+  .route("/:id/relatedproducts/:relatedproduct_id")
+  .delete(productHandler.removeRelatedProduct);
 
 export default router;
