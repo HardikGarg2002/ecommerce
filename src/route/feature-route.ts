50db5a187f2a8a8579392bf80e8c0a42d5c0ab75
hardikgarg2002 <hardikgarg3085@gmail.com>
2024-06-05 14:50:00 +0530

    refactor: feature routing

--- src/route/feature-route.ts
+++ src/route/feature-route.ts
@@ -1,26 +1,24 @@
 import express from "express";
-import * as featureService from "../service/feature-service";
+import * as featureHandler from "../handler/feature-handler";
 
 const router = express.Router();
 
-router.post("/", async (req, res) => {
-  try {
-    const feature = await featureService.create(req.body);
-    res.json(feature);
-  } catch (error) {
-    res.status(500).json({ message: error });
-  }
-});
+// //Route to get all features
+router.route("/").get(featureHandler.get);
 
-router.get("/", (req, res) => {
-  const allDisplayTags = featureService.get();
-  res.send(allDisplayTags);
-});
+// // Route to create a new feature
+router.route("/").post(featureHandler.create);
 
-router.get("/:id", (req, res) => {
-  const id = req.params.id;
-  const feature = featureService.getById(id);
-  res.status(201).json(feature);
-});
+//Route to get all subcategories based on searchText
+// router.route("/search").get(featureHandler.search);
+
+// //Route to get a feature by id
+router.route("/:id").get(featureHandler.getById);
+
+// //Route to edit a feature by id (only active) (also send meta data for the affected fields future prospective)
+router.route("/:id").patch(featureHandler.patch);
+
+// //Route to delete a feature by id     hard delete
+router.route("/:id").delete(featureHandler.remove);
 
 export default router;
