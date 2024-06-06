efebbcaa5308f66e22b4d20e0964743f69a215f9
hardikgarg2002 <hardikgarg3085@gmail.com>
2024-06-06 22:50:00 +0530

    feat: writting order apis

--- src/route/order-route.ts
+++ src/route/order-route.ts
@@ -1,52 +1,18 @@
+import * as orderHandler from "../handler/order-handler";
 import express from "express";
-import * as orderService from "../service/order-service";
 
 const router = express.Router();
 
-router.post("/", async (req, res) => {
-  try {
-    const order = await orderService.create(req.body);
-    res.json(order);
-  } catch (error) {
-    res.status(500).json({ message: error });
-  }
-});
+//Route to create an order
+router.route("/:id/status").patch(orderHandler.patchStatus);
 
-router.get("/", async (req, res) => {
-  try {
-    const orders = await orderService.get();
-    res.json(orders);
-  } catch (error) {
-    res.status(500).json({ message: error });
-  }
-});
+//Route to get all products
+router.route("/").get(orderHandler.get);
 
-router.get("/:id", async (req, res) => {
-  try {
-    const order = await orderService.getById(req.params.id);
-    res.json(order);
-  } catch (error) {
-    res.status(500).json({ message: error });
-  }
-});
+//Route to get a product by id
+router.route("/:id").get(orderHandler.getById);
 
-router.get("/user/:id", async (req, res) => {
-  try {
-    const orders = await orderService.getOrdersByUserId(req.params.id);
-    res.json(orders);
-  } catch (error) {
-    res.status(500).json({ message: error });
-  }
-});
-
-router.put("/:id/status", async (req, res) => {
-  try {
-    const status = req.body.status;
-    const order = await orderService.patchStatus(req.params.id, status);
-    res.json(order);
-  } catch (error) {
-    res.status(500).json({ message: error });
-  }
-});
+//Route to refresh payment
+router.route("/:id/payment/refresh").get(orderHandler.refreshStatus);
 
 export default router;
