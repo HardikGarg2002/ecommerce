5f0525c1c36511b723eeb1fac516d61205e12c6c
hardikgarg2002 <hardikgarg3085@gmail.com>
2024-07-22 15:00:00 +0530

    product handler layer create

--- src/handler/product-handler.ts
+++ src/handler/product-handler.ts
@@ -31,3 +31,23 @@ export const getById = async (
     next(err);
   }
 };
+
+export const create = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const user: IUser = req.body.loggedInUser;
+    const productInput: IProduct = req.body;
+    productInput.created = user;
+    productInput.updated = productInput.created;
+    if (!Array.isArray(productInput.tags)) {
+      productInput.tags = [];
+    }
+    const id = await productController.create(productInput);
+    res.status(201).json({ message: "Product created successfully", id });
+  } catch (err) {
+    next(err);
+  }
+};
