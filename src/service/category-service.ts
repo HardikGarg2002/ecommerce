e1dcd28631b7fd02ce127282934015125a917260
hardikgarg2002 <hardikgarg3085@gmail.com>
2024-10-01 16:50:10 +0530

    feat: category delete service

--- src/service/category-service.ts
+++ src/service/category-service.ts
@@ -1,25 +1,26 @@
-import Category from "../model/category";
+import Category from '../model/category';
 
 export async function create(category: any) {
-  const newCategory = new Category(category);
-  return await newCategory.save();
+	const newCategory = new Category(category);
+	return await newCategory.save();
 }
 
 export async function get() {
-  return await Category.find();
+	return await Category.find();
 }
 
 export async function getById(categoryId: string) {
-  return await Category.findById(categoryId);
+	return await Category.findById(categoryId);
 }
 
 export async function patch(categoryId: string, category: any): Promise<void> {
-  await Category.findByIdAndUpdate(categoryId, category);
+	await Category.findByIdAndUpdate(categoryId, category);
 }
 
-export async function patchStatus(
-  categoryId: string,
-  is_active: boolean
-): Promise<void> {
-  await Category.findByIdAndUpdate(categoryId, { is_active });
+export async function patchStatus(categoryId: string, is_active: boolean): Promise<void> {
+	await Category.findByIdAndUpdate(categoryId, { is_active });
+}
+
+export async function remove(categoryId: string): Promise<void> {
+	await Category.findByIdAndDelete(categoryId);
 }
