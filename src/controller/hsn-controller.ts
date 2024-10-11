4c89499ad5f93bd2a98f6f31745269cdc51ddda8
hardikgarg2002 <hardikgarg3085@gmail.com>
2024-10-12 04:00:00 +0530

    controller layer : hsn

--- src/controller/hsn-controller.ts
+++ src/controller/hsn-controller.ts
@@ -1,24 +1,38 @@
-import { IHsn } from "../interface/hsn";
-import * as hsnService from "../service/hsn-service";
+import { IHsn, IHsnWithMeta } from '../interface/hsn';
+import HsnService from '../service/hsn-service';
+import { IUser } from '../interface/user';
 
 export default class HsnController {
-  public get = async (): Promise<IHsn[]> => {
-    return await hsnService.get();
-  };
+	private _hsnService = new HsnService();
 
-  public getById = async (id: string): Promise<IHsn> => {
-    return await hsnService.getById(id?.trim());
-  };
+	public get = async (filters: any, pagination: any, sort: string): Promise<IHsnWithMeta> => {
+		return await this._hsnService.get(filters, pagination, sort);
+	};
 
-  public create = async (hsnInput: IHsn): Promise<string> => {
-    return (await hsnService.create(hsnInput))._id;
-  };
+	public getById = async (id: string): Promise<IHsn> => {
+		return await this._hsnService.getById(id?.trim());
+	};
 
-  public patch = async (id: string, hsnInput: Partial<IHsn>) => {
-    return await hsnService.patch(id.trim(), hsnInput);
-  };
+	public search = async (filters: any, pagination: any, sort: string, searchText: string): Promise<IHsnWithMeta> => {
+		return await this._hsnService.search(filters, pagination, sort, searchText.trim());
+	};
 
-  public activate = async (id: string, hsnInput: Partial<IHsn>) => {
-    return await hsnService.activate(id.trim(), hsnInput);
-  };
+	public create = async (hsnInput: IHsn): Promise<string> => {
+		hsnInput.code = hsnInput.code.trim();
+
+		return await this._hsnService.create(hsnInput);
+	};
+	public patch = async (
+		id: string,
+		user: IUser,
+		reason: string,
+		code?: string,
+		desc?: string,
+		gst?: number,
+	): Promise<void> => {
+		await this._hsnService.patch(id, user, reason, code?.trim(), desc?.trim(), gst);
+	};
+	public activate = async (id: string, user: IUser, reason: string, is_active: boolean): Promise<void> => {
+		await this._hsnService.activate(id, user, reason, is_active);
+	};
 }
