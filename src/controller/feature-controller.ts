1a831bd3f4b1531ceeb4a9b0e7f6c57718d995e0
hardikgarg2002 <hardikgarg3085@gmail.com>
2024-10-12 02:00:00 +0530

    controller layer : feature

--- src/controller/feature-controller.ts
+++ src/controller/feature-controller.ts
@@ -1,30 +1,40 @@
-import * as featureService from "../service/feature-service";
-import { IFeature } from "../interface/feature";
-import { IUser } from "../interface/user";
+import FeatureService from '../service/feature-service';
+import { IFeature, IFeatureWithMeta } from '../interface/feature';
+import { IUser } from '../interface/user';
+import ValidvalueService from '../service/validvalue-service';
 
 export default class FeatureController {
-  public get = async () => {
-    return await featureService.get();
-  };
+	private _featureService = new FeatureService();
+	private _validvalueService = new ValidvalueService();
 
-  public getById = async (id: string) => {
-    return await featureService.getById(id.trim());
-  };
+	public get = async (filters: any, pagination: any, sort: string) => {
+		return await this._featureService.get(filters, pagination, sort);
+	};
 
-  public create = async (featureInput: IFeature) => {
-    return await featureService.create(featureInput);
-  };
+	public getById = async (id: string) => {
+		return await this._featureService.getById(id.trim());
+	};
 
-  public patch = async (
-    id: string,
-    name?: string,
-    desc?: string,
-    sort?: number
-  ) => {
-    return await featureService.patch(id.trim(), { name, desc, sort });
-  };
+	public search = async (
+		filters: any,
+		pagination: any,
+		sort: string,
+		searchText: string,
+	): Promise<IFeatureWithMeta> => {
+		return await this._featureService.search(filters, pagination, sort, searchText.trim());
+	};
 
-  public remove = async (user: IUser, id: string, reason: string) => {
-    return await featureService.remove(id);
-  };
+	public create = async (featureInput: IFeature) => {
+		/// validate the type from the validvalue value key of type feature
+		await this._validvalueService.getValue('feature', featureInput.type);
+		return await this._featureService.create(featureInput);
+	};
+
+	public patch = async (user: IUser, id: string, reason: string, name?: string, desc?: string, sort?: number) => {
+		return await this._featureService.patch(user, id.trim(), reason.trim(), name?.trim(), desc?.trim(), sort);
+	};
+
+	public remove = async (user: IUser, id: string, reason: string) => {
+		return await this._featureService.remove(user, id.trim(), reason.trim());
+	};
 }
