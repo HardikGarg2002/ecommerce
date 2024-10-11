74b1fe5fbdfa439a6c180f08a1d0288f220977b5
hardikgarg2002 <hardikgarg3085@gmail.com>
2024-10-12 04:00:00 +0530

    controller layer : category

--- src/controller/category-controller.ts
+++ src/controller/category-controller.ts
@@ -1,43 +1,56 @@
-import * as categoryService from "../service/category-service";
-import { ICategory, ICategoryWithMeta } from "../interface/category";
-import { IUser } from "../interface/user";
+import CategoryService from '../service/category-service';
+import { ICategory, ICategoryWithMeta } from '../interface/category';
+import { IUser } from '../interface/user';
 
 export default class CategoryController {
-  public create = async (catInput: ICategory) => {
-    return await categoryService.create(catInput);
-  };
+	private _categoryService = new CategoryService();
 
-  public get = async () => {
-    const categories: ICategory[] = await categoryService.get();
-    return categories;
-  };
+	public create = async (catInput: ICategory) => {
+		return await this._categoryService.create(catInput);
+	};
 
-  public getById = async (id: string) => {
-    return await categoryService.getById(id.trim());
-  };
+	public get = async (filters: any, pagination: any, sort: string) => {
+		const categoriesWithMeta: ICategoryWithMeta = await this._categoryService.get(filters, pagination, sort);
+		return categoriesWithMeta;
+	};
 
-  public patch = async (
-    id: string,
-    reason: string,
-    user: IUser,
-    name?: string,
-    desc?: string,
-    img?: string,
-    sort?: number
-  ) => {
-    return await categoryService.patch(id.trim(), {
-      updated: user,
-      name: name?.trim(),
-      desc: desc?.trim(),
-      img: img?.trim(),
-      sort,
-    });
-  };
-  public remove = async (id: string) => {
-    return await categoryService.remove(id.trim());
-  };
+	public getById = async (id: string) => {
+		return await this._categoryService.getById(id.trim());
+	};
 
-  public patchStatus = async (id: string, active: boolean) => {
-    return await categoryService.patchStatus(id.trim(), active);
-  };
+	public search = async (
+		filters: any,
+		pagination: any,
+		sort: string,
+		searchText: string,
+	): Promise<ICategoryWithMeta> => {
+		return await this._categoryService.search(filters, pagination, sort, searchText.trim());
+	};
+
+	public patch = async (
+		id: string,
+		reason: string,
+		user: IUser,
+		name?: string,
+		desc?: string,
+		img?: string,
+		sort?: number,
+	) => {
+		return await this._categoryService.patch(
+			id.trim(),
+			reason.trim(),
+			user,
+			name?.trim(),
+			desc?.trim(),
+			img?.trim(),
+			sort,
+		);
+	};
+	public remove = async (id: string, reason: string, user: IUser) => {
+		return await this._categoryService.remove(id.trim(), reason.trim(), user);
+	};
+
+	public activate = async (id: string, reason: string, user: IUser, active: boolean) => {
+		return await this._categoryService.activate(id.trim(), reason.trim(), user, active);
+	};
 }
