357d92c43db7e402bae760b1b7eab817c9588952
hardikgarg2002 <hardikgarg3085@gmail.com>
2024-10-12 16:00:00 +0530

    controller layer : tag

--- src/controller/tag-controller.ts
+++ src/controller/tag-controller.ts
@@ -1,19 +1,27 @@
-import { ITag } from '../interface/tag';
-import * as tagService from '../service/tag-service';
+import { ITag, ITagWithMeta } from '../interface/tag';
+import TagService from '../service/tag-service';
+import { IUser } from '../interface/user';
 
 export default class TagController {
+	private _tagService = new TagService();
+
 	public create = async (tagInput: ITag): Promise<string> => {
-		return (await tagService.create(tagInput))._id;
+		return await this._tagService.create(tagInput);
 	};
 
-	public get = async (): Promise<ITag[]> => {
-		return await tagService.get();
+	public get = async (filters: any, pagination: any, sort: string): Promise<ITagWithMeta> => {
+		return await this._tagService.get(filters, pagination, sort);
 	};
 
 	public getById = async (id: string): Promise<ITag> => {
-		return await tagService.getById(id?.trim());
+		return await this._tagService.getById(id?.trim());
 	};
-	public activate = async (id: string, is_active: boolean): Promise<void> => {
-		await tagService.patchStatus(id, is_active);
+
+	public search = async (filters: any, pagination: any, sort: string, searchText: string): Promise<ITagWithMeta> => {
+		return await this._tagService.search(filters, pagination, sort, searchText.trim());
+	};
+
+	public activate = async (id: string, user: IUser, reason: string, is_active: boolean): Promise<void> => {
+		await this._tagService.activate(id, user, reason, is_active);
 	};
 }
