c7309fe2c68537e7b85874ac563f098f38330241
hardikgarg2002 <hardikgarg3085@gmail.com>
2024-10-14 16:00:00 +0530

    feat: tag service with all validations

--- src/service/tag-service.ts
+++ src/service/tag-service.ts
@@ -1,35 +1,112 @@
-import Tag from "../model/tag";
+import { ITag, ITagWithMeta } from '../interface/tag';
+import Tag from '../model/tag';
+import { craftPagination, craftQuery, craftSort } from 'filter-library';
+import { APIError } from '@hardikgarg2002/node-errorify';
+import { IUser } from '../interface/user';
 
-export async function create(tag: any) {
-  const newTag = new Tag(tag);
-  return await newTag.save();
-}
+export default class TagService {
+	public async getById(id: string, statusError: boolean = false): Promise<ITag> {
+		const tag: ITag | null = await Tag.findById(id).lean();
+		if (!tag) throw APIError.BusinessError(`Tag does not exist by this id ${id}`, `ERR_NOT_FOUND`);
+		if (statusError === true && tag.is_active === false) {
+			throw APIError.BusinessError('Tag Inactive', 'ERR_INACTIVE_TAG');
+		}
+		return tag;
+	}
 
-export async function get() {
-  return await Tag.find();
-}
+	public async get(filters: any, pagination: any, sort: string): Promise<ITagWithMeta> {
+		const criteria = craftQuery(filters);
+		const { skip, limit } = craftPagination(pagination.page, pagination.pageSize);
+		const sortOptions = craftSort(sort);
+		const tagList: ITag[] = await Tag.find(criteria)
+			.sort(sortOptions as any)
+			.collation({ locale: 'en' })
+			.skip(skip)
+			.limit(limit);
+		const totalCount = await Tag.countDocuments(criteria);
 
-export async function getById(tagId: string) {
-  const tag = await Tag.findById(tagId);
-  if (!tag) {
-    throw new Error("Tag not found");
-  }
-  return tag;
-}
+		const data: ITagWithMeta = {
+			data: tagList,
+			meta: {
+				pagination: {
+					page: skip / limit + 1,
+					pageSize: limit,
+					pageCount: Math.ceil(totalCount / limit),
+					total: totalCount,
+				},
+			},
+		};
+		return data;
+	}
 
-export async function patch(tagId: string, tagInput: any): Promise<void> {
-  await Tag.findByIdAndUpdate(tagId, tagInput);
-}
+	public async save(input: ITag, isNew: boolean = false): Promise<ITag> {
+		const tag = new Tag(input);
+		tag.isNew = isNew;
+		return (await tag.save()).toObject();
+	}
 
-export async function remove(tagId: string): Promise<void> {
-  await Tag.findByIdAndDelete(tagId);
-}
+	public search = async (filters: any, pagination: any, sort: string, searchText: string): Promise<ITagWithMeta> => {
+		const criteria = craftQuery(filters);
+		const { skip, limit } = craftPagination(pagination.page, pagination.pageSize);
+		const sortOptions = craftSort(sort);
+		const searchCriteria = {
+			$or: [
+				{ text: { $regex: searchText, $options: 'i' } },
+				{ slug: { $regex: searchText, $options: 'i' } },
+				{ 'updated.name': { $regex: searchText, $options: 'i' } },
+			],
+		};
 
-export async function patchStatus(
-  tagId: string,
-  status: boolean
-): Promise<void> {
-  await Tag.findByIdAndUpdate(tagId, {
-    is_active: status,
-  });
+		const filterSearch = { ...criteria, ...searchCriteria };
+
+		const tagList: ITag[] = await Tag.find(filterSearch)
+			.sort(sortOptions as any)
+			.collation({ locale: 'en' })
+			.skip(skip)
+			.limit(limit)
+			.lean();
+
+		const totalCount = await Tag.countDocuments(filterSearch);
+
+		const data: ITagWithMeta = {
+			data: tagList,
+			meta: {
+				pagination: {
+					page: skip / limit + 1,
+					pageSize: limit,
+					pageCount: Math.ceil(totalCount / limit),
+					total: totalCount,
+				},
+			},
+		};
+		return data;
+	};
+
+	public async create(tagInput: ITag): Promise<string> {
+		const slug = tagInput.text.replace(/[^\w]/g, '').toLowerCase();
+		await this.checkDuplicates(slug);
+		tagInput.slug = slug;
+		const newTag: ITag = await this.save(tagInput, true);
+
+		return newTag._id!;
+	}
+
+	public async activate(id: string, updated: IUser, reason: string, is_active: boolean): Promise<void> {
+		const existingTag: ITag = await this.getById(id);
+		const updatedTag: ITag = { ...existingTag, is_active, updated };
+		if (is_active !== existingTag.is_active) {
+			const savedTag: ITag = await this.save(updatedTag, false);
+		}
+	}
+
+	public async checkDuplicates(slug: string): Promise<void> {
+		if (await Tag.exists({ slug })) {
+			throw APIError.BusinessError('Tag with the same text already exists in the Database', 'Err_DUPLICATES');
+		}
+	}
+
+	public makeSortCriteria(sort: string): any {
+		const [field, sortOption] = sort.split(':');
+		return { [field]: sortOption };
+	}
 }
