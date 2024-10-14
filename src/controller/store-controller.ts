36361b29687c99459aef1b7915726525d5ad6efb
hardikgarg2002 <hardikgarg3085@gmail.com>
2024-10-14 12:00:00 +0530

    feat: store apis to create,get, patch, add and remove

--- src/controller/store-controller.ts
+++ src/controller/store-controller.ts
@@ -1,40 +1,61 @@
-import { IStore, IStoreWithMeta } from "../interface/store";
-import * as _storeService from "../service/store-service";
-import { IUser } from "../interface/user";
+import { IStore, IStoreWithMeta } from '../interface/store';
+import StoreService from '../service/store-service';
+import { IUser } from '../interface/user';
+import ValidvalueService from '../service/validvalue-service';
+import { APIError } from '@hardikgarg2002/node-errorify';
 
 export default class StoreController {
-  public get = async (): Promise<IStore[]> => {
-    return await _storeService.get();
-  };
+	private _storeService = new StoreService();
+	private _validvalueService = new ValidvalueService();
 
-  public getById = async (storeId: string): Promise<IStore> => {
-    return await _storeService.getById(storeId.trim());
-  };
+	public get = async (filters: any, pagination: any, sort: string): Promise<IStoreWithMeta> => {
+		return await this._storeService.get(filters, pagination, sort);
+	};
 
-  public create = async (storeInput: IStore): Promise<string> => {
-    return await _storeService.create(storeInput);
-  };
+	public getById = async (storeId: string): Promise<IStore> => {
+		return await this._storeService.getById(storeId.trim());
+	};
 
-  public patch = async (
-    storeId: string,
-    user: IUser,
-    name?: string,
-    desc?: string,
-    city_key?: string,
-    sort?: number
-  ) => {
-    // Validate city key if it comes in request
+	public search = async (filters: any, pagination: any, sort: string, searchText: string): Promise<IStoreWithMeta> => {
+		return await this._storeService.search(filters, pagination, sort, searchText.trim());
+	};
 
-    return await _storeService.patch(storeId.trim(), {
-      updated: user,
-      name: name?.trim(),
-      desc: desc?.trim(),
-      city_key: city_key?.trim(),
-      sort,
-    });
-  };
+	public create = async (storeInput: IStore): Promise<string> => {
+		const city = await this._validvalueService.getValue('city', storeInput.city_key);
+		if (city.is_active === false) {
+			throw APIError.BusinessError('City is inactive', 'ERR_INACTIVE');
+		}
+		return await this._storeService.create(storeInput);
+	};
 
-  public patchStatus = async (storeId: string, is_active: boolean) => {
-    return await _storeService.patchStatus(storeId.trim(), is_active);
-  };
+	public patch = async (
+		storeId: string,
+		user: IUser,
+		reason: string,
+		name?: string,
+		desc?: string,
+		city_key?: string,
+		sort?: number,
+	) => {
+		if (city_key) {
+			const city = await this._validvalueService.getValue('city', city_key);
+			if (city.is_active === false) {
+				throw APIError.BusinessError('City is inactive', 'ERR_CITY_INACTIVE');
+			}
+		}
+
+		return await this._storeService.patch(
+			storeId.trim(),
+			user,
+			reason.trim(),
+			name?.trim(),
+			desc?.trim(),
+			city_key?.trim(),
+			sort,
+		);
+	};
+
+	public activate = async (storeId: string, user: IUser, reason: string, is_active: boolean) => {
+		return await this._storeService.activate(storeId.trim(), user, reason.trim(), is_active);
+	};
 }
--- src/service/store-service.ts
+++ src/service/store-service.ts
@@ -1,29 +1,129 @@
-import { IStore } from "../interface/store";
-import Store from "../model/store";
+import Store from '../model/store';
+import { craftPagination, craftQuery, craftSort } from 'filter-library';
+import { APIError } from '@hardikgarg2002/node-errorify';
+import { IStore, IStoreWithMeta } from '../interface/store';
+import { IUser } from '../interface/user';
 
-export async function create(data: IStore) {
-  const store = await Store.create(data);
-  return store._id;
-}
+export default class StoreService {
+	public async _validateNameAndCodeUnique(name: string, code?: string) {
+		const matchedStoreCount = await Store.countDocuments({
+			$or: [
+				{ name: { $regex: new RegExp(`^${name?.trim()}$`), $options: 'i' } },
+				{ code: { $regex: new RegExp(`^${code?.trim()}$`), $options: 'i' } },
+			],
+		});
+		if (matchedStoreCount > 0) {
+			throw APIError.BusinessError('store with the same name and code already exists in database', 'ERR_DUPLICATE');
+		}
+	}
+	public async save(storeInput: IStore, isNew: boolean = false): Promise<IStore> {
+		const store = new Store(storeInput);
+		store.isNew = isNew;
+		return (await store.save()).toObject();
+	}
+	public async get(filters: any, pagination: any, sort: string): Promise<IStoreWithMeta> {
+		const criteria = craftQuery(filters);
+		const { skip, limit } = craftPagination(pagination.page, pagination.pageSize);
+		const sortOptions = craftSort(sort);
 
-export async function get(): Promise<IStore[]> {
-  return await Store.find();
-}
+		const storeList: IStore[] = await Store.find(criteria)
+			.sort(sortOptions as any)
+			.collation({ locale: 'en' })
+			.skip(skip)
+			.limit(limit);
+		const totalCount = await Store.countDocuments(criteria);
+		const data: IStoreWithMeta = {
+			data: storeList,
+			meta: {
+				pagination: {
+					page: skip / limit + 1,
+					pageSize: limit,
+					pageCount: Math.ceil(totalCount / limit),
+					total: totalCount,
+				},
+			},
+		};
+		return data;
+	}
+	public async getById(storeId: string): Promise<IStore> {
+		const store = await Store.findById(storeId).lean();
+		if (!store) {
+			throw APIError.BusinessError('store not found', 'ERR_NOT_FOUND');
+		}
+		return store;
+	}
 
-export async function getById(id: string): Promise<IStore> {
-  const store = await Store.findById(id);
-  if (!store) throw Error("Store not found");
-  return store;
-}
+	public search = async (filters: any, pagination: any, sort: string, searchText: string): Promise<IStoreWithMeta> => {
+		const criteria = craftQuery(filters);
+		const { skip, limit } = craftPagination(pagination.page, pagination.pageSize);
+		const sortOptions = craftSort(sort);
+		const searchCriteria = {
+			$or: [
+				{ name: { $regex: searchText, $options: 'i' } },
+				{ desc: { $regex: searchText, $options: 'i' } },
+				{ 'updated.name': { $regex: searchText, $options: 'i' } },
+			],
+		};
 
-export async function patch(id: string, data: Partial<IStore>) {
-  const store = await Store.findByIdAndUpdate(id, data);
-  if (!store) throw Error("Store not found");
-  return store;
-}
+		const filterSearch = { ...criteria, ...searchCriteria };
 
-export async function patchStatus(id: string, isActive: boolean) {
-  const store = await Store.findByIdAndUpdate(id, { is_active: isActive });
-  if (!store) throw Error("Store not found");
-  return store;
+		const storeList: IStore[] = await Store.find(filterSearch)
+			.sort(sortOptions as any)
+			.collation({ locale: 'en' })
+			.skip(skip)
+			.limit(limit)
+			.lean();
+
+		const totalCount = await Store.countDocuments(filterSearch);
+
+		const data: IStoreWithMeta = {
+			data: storeList,
+			meta: {
+				pagination: {
+					page: skip / limit + 1,
+					pageSize: limit,
+					pageCount: Math.ceil(totalCount / limit),
+					total: totalCount,
+				},
+			},
+		};
+		return data;
+	};
+
+	public async create(storeInput: IStore): Promise<string> {
+		await this._validateNameAndCodeUnique(storeInput.name, storeInput.code);
+		const newStore: IStore = await this.save(storeInput, true);
+		return newStore._id!;
+	}
+
+	public async patch(
+		storeId: string,
+		user: IUser,
+		reason: string,
+		name?: string,
+		desc?: string,
+		city_key?: string,
+		sort?: number,
+		is_active?: boolean,
+	): Promise<void> {
+		const existingStore = await this.getById(storeId);
+		// if (is_active === undefined && !existingStore.is_active) {
+		// 	throw APIError.BusinessError(`Inactive Store can not be modified`, 'ERR_STORE_INACTIVE');
+		// }
+		name && existingStore.name.toLowerCase() !== name.toLowerCase() && (await this._validateNameAndCodeUnique(name));
+		const updatedStore: IStore = {
+			...existingStore,
+			name: name ?? existingStore.name,
+			desc: desc ?? existingStore.desc,
+			city_key: city_key ?? existingStore.city_key,
+			sort: sort ?? existingStore.sort,
+			is_active: is_active ?? existingStore.is_active,
+			updated: user,
+		};
+		await this.save(updatedStore);
+	}
+
+	public async activate(storeId: string, user: IUser, reason: string, is_active: boolean) {
+		await this.patch(storeId, user, reason, undefined, undefined, undefined, undefined, is_active);
+	}
 }
