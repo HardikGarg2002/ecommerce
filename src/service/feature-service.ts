e7cfe08617f7d5478c00cb7fb16f3934fbd262c3
hardikgarg2002 <hardikgarg3085@gmail.com>
2024-10-15 16:30:00 +0530

    feat: feature service get and search apis

--- src/service/feature-service.ts
+++ src/service/feature-service.ts
@@ -1,26 +1,104 @@
-import { IFeature } from "../interface/feature";
-import Feature from "../model/feature";
+import { craftPagination, craftQuery, craftSort } from 'filter-library';
+import Feature from '../model/feature';
+import { IFeature, IFeatureWithMeta } from '../interface/feature';
+import { IUser } from '../interface/user';
+import ProductService from './product-service';
+import { APIError } from '@hardikgarg2002/node-errorify';
 
-export async function create(feature: any) {
-  const newFeature = new Feature(feature);
-  return await newFeature.save();
-}
+export default class FeatureService {
+	private _productService = new ProductService();
+	private allowedQueryFields = ['name', 'desc', 'code', 'type', 'sort', 'created.name', 'updated.name'];
 
-export async function get(): Promise<IFeature[]> {
-  const features = await Feature.find();
-  return features;
-}
+	private async save(featureInput: IFeature, isNew: boolean = false): Promise<IFeature> {
+		const feature = new Feature(featureInput);
+		feature.isNew = isNew;
+		return (await feature.save()).toObject();
+	}
 
-export async function getById(id: string): Promise<IFeature> {
-  const feature = await Feature.findById(id);
-  if (!feature) throw new Error("Feature not found");
-  return feature;
-}
+	public async get(filters: any, pagination: any, sort: string): Promise<IFeatureWithMeta> {
+		const criteria = craftQuery(filters, this.allowedQueryFields);
+		const { skip, limit } = craftPagination(pagination.page, pagination.pageSize);
+		const sortOptions = craftSort(sort);
 
-export async function patch(id: string, featureInput: Partial<IFeature>) {
-  await Feature.findByIdAndUpdate(id, featureInput);
-}
+		const featuresList: IFeature[] = await Feature.find(criteria)
+			.sort(sortOptions as any)
+			.collation({ locale: 'en' })
+			.skip(skip)
+			.limit(limit);
+		const totalCount = await Feature.countDocuments(criteria);
 
-export async function remove(categoryId: string): Promise<void> {
-  await Feature.findByIdAndDelete(categoryId);
+		const data: IFeatureWithMeta = {
+			data: featuresList,
+			meta: {
+				pagination: {
+					page: skip / limit + 1,
+					pageSize: limit,
+					pageCount: Math.ceil(totalCount / limit),
+					total: totalCount,
+				},
+			},
+		};
+		return data;
+	}
+
+	public async getById(id: string): Promise<IFeature> {
+		const feature = await Feature.findById(id).lean();
+		if (!feature) {
+			throw APIError.BusinessError('feature not found', 'ERR_NOT_FOUND');
+		}
+
+		return feature;
+	}
+
+	public async getByCode(code: string): Promise<IFeature> {
+		const feature = await Feature.findOne({ code }).lean();
+		if (!feature) {
+			throw APIError.BusinessError(`feature with ${code} not found`, 'ERR_NOT_FOUND');
+		}
+
+		return feature;
+	}
+
+	public search = async (
+		filters: any,
+		pagination: any,
+		sort: string,
+		searchText: string,
+	): Promise<IFeatureWithMeta> => {
+		const criteria = craftQuery(filters, this.allowedQueryFields);
+		const { skip, limit } = craftPagination(pagination.page, pagination.pageSize);
+		const sortOptions = craftSort(sort);
+
+		const searchCriteria = {
+			$or: [
+				{ name: { $regex: searchText, $options: 'i' } },
+				{ desc: { $regex: searchText, $options: 'i' } },
+				{ 'updated.name': { $regex: searchText, $options: 'i' } },
+			],
+		};
+
+		const filterSearch = { ...criteria, ...searchCriteria };
+
+		const featureList: IFeature[] = await Feature.find(filterSearch)
+			.sort(sortOptions as any)
+			.collation({ locale: 'en' })
+			.skip(skip)
+			.limit(limit)
+			.lean();
+
+		const totalCount = await Feature.countDocuments(filterSearch);
+
+		const data: IFeatureWithMeta = {
+			data: featureList,
+			meta: {
+				pagination: {
+					page: skip / limit + 1,
+					pageSize: limit,
+					pageCount: Math.ceil(totalCount / limit),
+					total: totalCount,
+				},
+			},
+		};
+		return data;
+	};
 }
